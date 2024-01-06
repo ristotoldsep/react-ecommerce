950e11dfd0be5655950b264ff58670f476e00f83
Risto Tõldsep <risto@MacBook-Pro.local>
2024-01-06 17:30:58 +0200

    Added basic routing with React Router

---
+++ src/routes/layout/layout.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import './layout.styles.scss';
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+
+const Layout = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+            <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+                SHOP
+            </Link>
+        </div>
+      </div>
+      <Outlet />
+      <div className="footer">
+        <h2>Footer &copy; 2024</h2>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Layout;
