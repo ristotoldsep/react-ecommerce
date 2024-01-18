72adfcd09f11c151adbc39874f11290491451657
Risto Tõldsep <risto@MacBook-Pro.local>
2024-01-18 20:14:55 +0200

    Seeded Firebase db with data and started pulling it to front-end

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -14,8 +14,12 @@ import {
   getFirestore,
   doc,
   getDoc,
-  setDoc
-} from 'firebase/firestore'
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -42,6 +46,30 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 // Initiate db
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('Collection added successfully.');
+}
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+
+  const categoryMap = querySnapshot.docs.reduce(() => {}, {});
+}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
   if (!userAuth) return;
