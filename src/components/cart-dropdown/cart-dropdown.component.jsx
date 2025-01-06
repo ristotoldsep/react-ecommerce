fcdccf84f2ad0b2afc201b39de4c7cdb0e2f1c6c
Risto Tõldsep <r.toldsep@gmail.com>
2025-01-06 22:55:22 +0200

    Fixed cart closing

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -22,7 +22,7 @@ const CartDropdown = () => {
   };
 
   return (
-    <CartDropdownContainer>
+    <CartDropdownContainer className="cart-dropdown-container">
       <CartItems>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
